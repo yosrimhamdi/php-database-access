ee8873d205e4100bc5c08ba9230b7e9224c3346b
yosrimhamdi <yosrimhamdi@outlook.com>
2021-11-02 15:00:22 +0100

    test

--- index.php
+++ index.php
@@ -1,55 +1,19 @@
 <?php
-session_start();
-
-if (isset($_SESSION["status"])) {
-  echo "<div class=\"alert alert-$_SESSION[status]\" role=\"alert\">";
-  echo $_SESSION["message"];
-  echo "</div>";
+function pr($str) {
+  echo "$str \n";
 }
 
-session_unset();
-session_destroy();
-?>
+function solve($equation) {
+  pr($equation);
 
-<!DOCTYPE html>
-<html lang="en">
+  $equation = str_replace(" ", "", $equation);
 
-  <head>
-    <meta charset="UTF-8">
-    <meta
-      http-equiv="X-UA-Compatible"
-      content="IE=edge"
-    >
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0"
-    >
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-      crossorigin="anonymous"
-    >
-    <title>Document</title>
-  </head>
+  preg_match_all("/([-+]?)(\d+)(x\^2|x|[-+]?|$)/", $equation, $parts);
 
-  <body>
-    <form
-      action="/submit-prep-state.php"
-      method="POST"
-    >
-      <p>User Name:</p>
-      <input
-        type="text"
-        name="user_name"
-      />
-      <p>User Age:</p>
-      <input
-        type="text"
-        name="user_age"
-      />
-      <button type="submit">submit</button>
-    </form>
-  </body>
+  echo "<pre>";
+  print_r($parts);
+  echo "</pre>";
+}
 
-</html>
+// solve("+343x^2 + 2x - 1");
+solve("343x^2 + 2x - 1");
